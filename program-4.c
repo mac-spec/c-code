28ea79327e1320ff6af7a590b483c00dd699366f
Riya Raj <lorven.riya.feb2007@gmail.com>
2026-02-05 13:33:57 +0530

    program-4

---
+++ program-4.c
@@ -0,0 +1,12 @@
+// checks if the number is even or odd
+#include<stdio.h>
+int main()
+{
+int a;
+printf("Enter a number to check even or odd :\n");
+scanf("%d",&a);
+if(a%2==0)
+printf("%d is Even",a);
+else
+printf("%d is Odd",a);
+}
