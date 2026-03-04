3fb69c4f6ae60661f0b6b9708964e75b2c425ff4
mac-spec <lorven.riya.feb2007@gmail.com>
2026-03-04 21:24:23 +0530

    Basic string code with strcpy function

--- program-33.c
+++ program-33.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
-#include<string.h>
+#include<string.h>//invoking string functions
 int main()
 {
-char s1[10] ="Friends!!";
+char s1[10] ="Friends!!";//Storing the original value
 printf("The original string\n");
 printf("%s\n",s1);
 char s2[10];
-strcpy(s2,s1);
+strcpy(s2,s1);//storing copy of the original value
 printf("The copied string\n");
 printf("%s",s2);
 return 0;
