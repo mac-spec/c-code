94558801e68a3c1eae6caf52bfbd9dd13ce09a36
mac-spec <lorven.riya.feb2007@gmail.com>
2026-04-08 21:28:35 +0530

    Time: 3 ms (60.84%), Space: 8.5 MB (28.71%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.c
@@ -0,0 +1,13 @@
+int fib(int n) {
+    // Base cases
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    int a = 0,b=1,res;
+    for (int i = 2; i <= n; i++) {
+        res = a + b;
+        a = b;   
+        b = res; 
+    }
+
+    return b;
+}
