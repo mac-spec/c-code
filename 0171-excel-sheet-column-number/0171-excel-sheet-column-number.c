3540227a8fa9f9a10a07b008c31e5279eb0156b0
mac-spec <lorven.riya.feb2007@gmail.com>
2026-04-11 20:27:44 +0530

    Time: 0 ms (100%), Space: 9.2 MB (29.35%) - LeetHub

---
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.c
@@ -0,0 +1,8 @@
+int titleToNumber(char* columnTitle) {
+      long result = 0; 
+    for (int i = 0; columnTitle[i] != '\0'; i++) {
+        int value = columnTitle[i] - 'A' + 1;
+        result = result * 26 + value;
+    }
+    return (int)result;
+}
