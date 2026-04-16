9c291c768bb48dfc473e275d381966aaa08fa2be
mac-spec <lorven.riya.feb2007@gmail.com>
2026-04-16 12:24:58 +0530

    Time: 2 ms (31.44%), Space: 15.8 MB (74.35%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.c
@@ -0,0 +1,16 @@
+int maxProfit(int* prices, int pricesSize) {
+    int min=prices[0];
+    int max=0;
+ 
+    for(int i=0;i<pricesSize;i++)
+    {
+        if (prices[i] < min) {
+            min = prices[i];
+        } 
+                 else if (prices[i]-min>max) 
+                 {
+            max = prices[i] - min;
+        }
+    }
+    return max;
+}
